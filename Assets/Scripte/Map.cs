691f3c8c94346ec5c4380a3bd05fc917f0199ab8
popote <yvan.bladet@gmail.com>
2020-12-07 23:30:55 +0100

    AddProject

---
+++ Assets/Scripte/Map.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+public class Map : MonoBehaviour {
+
+    [Header("Prefabs")] 
+    public GameObject BlocPrefab;
+
+    [Header("Parameters")] 
+    public float Modifier;
+    public int MapXSize, MapZSize;
+    public int GapX, GapZ;
+
+    [Header("References")] 
+    public Camera Camera;
+    
+    private float[,] _matrix;
+    private readonly List<GameObject> _blocs = new List<GameObject>();
+
+
+    public void Generate() {
+        ClearMap();
+        SetupCamera();
+        BlocsGeneration();
+    }
+
+    public void SetupCamera() {
+        Camera.transform.position = new Vector3((float) MapXSize / 2, Camera.transform.position.y, (float) MapZSize / 2);
+    }
+    
+    public void BlocsGeneration()
+    {
+        _matrix = new float[MapXSize,MapZSize];
+        _matrix = PerlinNoise.Cave(_matrix, Modifier, true);
+        for (int i = 0; i < MapXSize; i++) {
+            for (int j = 0; j < MapZSize; j++)
+            {
+                float value = _matrix[i, j];
+                GameObject instantiate = Instantiate(BlocPrefab, new Vector3(i * GapX, 0, j * GapZ), Quaternion.identity, transform);
+                instantiate.GetComponent<MeshRenderer>().material.color = Color.white * value;
+                _blocs.Add(instantiate);
+            }
+        }
+    }
+
+    public void ClearMap() {
+        foreach (GameObject bloc in _blocs) {
+            Destroy(bloc);
+        }
+        _blocs.Clear();
+    }
+
+}
---
+++ Assets/Scripte/PerlinNoise.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Utils {
+    public static class PerlinNoise {
+    
+        public static float[,] Cave(float[,] map, float modifier, bool edgesAreWalls) {
+            for (int x = 0; x < map.GetUpperBound(0); x++) {
+                for (int y = 0; y < map.GetUpperBound(1); y++) {
+                    if (edgesAreWalls && (x == 0 || y == 0 || x == map.GetUpperBound(0) - 1 || y == map.GetUpperBound(1) - 1)) {
+                        map[x, y] = 1; //Keep the edges as walls
+                    }
+                    else {
+                        //Generate a new point using Perlin noise, then round it to a value of either 0 or 1
+                        map[x, y] = Mathf.PerlinNoise(x * modifier, y * modifier);
+                    }
+                }
+            }
+            return map;
+        }
+
+    }
+}
