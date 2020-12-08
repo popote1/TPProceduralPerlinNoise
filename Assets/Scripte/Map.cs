de844fc868141ebea5b472772f10a7db79e8d249
popote <yvan.bladet@gmail.com>
2020-12-08 17:29:12 +0100

    sfd

--- Assets/Scripte/Map.cs
+++ Assets/Scripte/Map.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Utils;
 
@@ -8,14 +9,18 @@ public class Map : MonoBehaviour {
     public GameObject BlocPrefab;
 
     [Header("Parameters")] 
+    [Range(0,1)]public float ThresHold=0.5f;
     public float Modifier;
     public int MapXSize, MapZSize;
     public int GapX, GapZ;
+    public Vector2 Offset;
 
     [Header("References")] 
     public Camera Camera;
-    
-    private float[,] _matrix;
+
+    public Cell[,] Matrix;
+    private List<List<Cell>> _rooms;
+    private float[,] _perlinMatrix;
     private readonly List<GameObject> _blocs = new List<GameObject>();
 
 
@@ -25,21 +30,101 @@ public class Map : MonoBehaviour {
         BlocsGeneration();
     }
 
+    /// <summary>
+    /// Code teste
+    /// </summary>
+
+    void StartAnalyse()
+    {
+        StartCoroutine(Analyse());
+    }
+
+    IEnumerator<List<Cell>> Analyse(Cell cell )
+    {
+        List<Cell>roomCell = new List<Cell>();
+        List<Cell>cellCheck=new List<Cell>();
+        roomCell.Add(cell);
+        Debug.Log("Anilise de la room");
+        
+        while (roomCell.Count>=cellCheck.Count)
+        {
+            Debug.Log(roomCell.Count+" case font partie de la salle   "+cellCheck.Count+"   On était traiter");
+            
+            List<Cell> newRoomCells= new List<Cell>();
+            foreach (Cell analisCell in roomCell)
+            {
+                Debug.Log("la casse traiter a comme coordoner" + analisCell.Position);
+                if (!cellCheck.Contains(analisCell))
+                {
+                    if (analisCell.Position.y+1 <MapXSize)
+                    {
+                        if (Matrix[analisCell.Position.x, analisCell.Position.y + 1].Material.color == Color.white && !roomCell.Contains(Matrix[analisCell.Position.x, analisCell.Position.y + 1])&& !newRoomCells.Contains(Matrix[analisCell.Position.x, analisCell.Position.y + 1]))
+                        {
+                            newRoomCells.Add(Matrix[analisCell.Position.x, analisCell.Position.y]);
+                        }
+                    }
+                    
+                    if (analisCell.Position.y - 1 >= 0)
+                    {
+                        if (Matrix[analisCell.Position.x, analisCell.Position.y - 1].Material.color == Color.white && !roomCell.Contains(Matrix[analisCell.Position.x, analisCell.Position.y - 1])&& !newRoomCells.Contains(Matrix[analisCell.Position.x, analisCell.Position.y - 1]))
+                        {
+                            newRoomCells.Add(Matrix[analisCell.Position.x, analisCell.Position.y]);
+                        }
+                    }
+
+                    if (analisCell.Position.x + 1 < MapZSize)
+                    {
+                        if (Matrix[analisCell.Position.x + 1, analisCell.Position.y].Material.color == Color.white && !roomCell.Contains(Matrix[analisCell.Position.x + 1, analisCell.Position.y])&& !newRoomCells.Contains(Matrix[analisCell.Position.x+1, analisCell.Position.y ]))
+                        {
+                            newRoomCells.Add(Matrix[analisCell.Position.x, analisCell.Position.y]);
+                        }
+                    }
+
+                    if (analisCell.Position.x - 1 >= 0)
+                    {
+                        if (Matrix[analisCell.Position.x - 1, analisCell.Position.y - 1].Material.color == Color.white && !roomCell.Contains(Matrix[analisCell.Position.x - 1, analisCell.Position.y])&& !newRoomCells.Contains(Matrix[analisCell.Position.x-1, analisCell.Position.y ]))
+                        {
+                            newRoomCells.Add(Matrix[analisCell.Position.x, analisCell.Position.y]);
+                        }
+                    }
+
+                    cellCheck.Add(analisCell);
+                    yield return new WaitForSeconds(0.1f);
+                }
+            }
+
+            foreach (Cell newRoomCell in newRoomCells)
+            {
+                roomCell.Add(newRoomCell);
+            }
+            newRoomCells.Clear();
+x        }
+    }
+
     public void SetupCamera() {
         Camera.transform.position = new Vector3((float) MapXSize / 2, Camera.transform.position.y, (float) MapZSize / 2);
     }
     
     public void BlocsGeneration()
     {
-        _matrix = new float[MapXSize,MapZSize];
-        _matrix = PerlinNoise.Cave(_matrix, Modifier, true);
+        Matrix = new Cell[MapXSize,MapZSize];
+        _perlinMatrix = PerlinNoise.Cave(new float[MapXSize,MapZSize], Modifier, true,Offset);
         for (int i = 0; i < MapXSize; i++) {
             for (int j = 0; j < MapZSize; j++)
             {
-                float value = _matrix[i, j];
+                float value = _perlinMatrix[i, j];
                 GameObject instantiate = Instantiate(BlocPrefab, new Vector3(i * GapX, 0, j * GapZ), Quaternion.identity, transform);
-                instantiate.GetComponent<MeshRenderer>().material.color = Color.white * value;
+                if (value <ThresHold)
+                {
+                    instantiate.GetComponent<MeshRenderer>().material.color = Color.white;
+                }
+                else
+                {
+                    instantiate.GetComponent<MeshRenderer>().material.color = Color.black;
+                }
+
                 _blocs.Add(instantiate);
+                Matrix[i,j]=new Cell(new Vector2Int(i,j),instantiate);
             }
         }
     }
@@ -51,4 +136,121 @@ public class Map : MonoBehaviour {
         _blocs.Clear();
     }
 
+    public void RoomDetector()
+    {
+        List<Cell> cellTraiter = new List<Cell>();
+        Color color = Color.blue;
+        for (int x = 0; x < MapXSize; x++) {
+            for (int y = 0; y < MapZSize; y++) {
+                if (!cellTraiter.Contains(Matrix[x, y])) {
+                    if (Matrix[x,y].Material.color==Color.white) {
+                        Debug.Log("La case "+x+" ; "+y);
+                        RoomAnaliser(Matrix[x,y],out List<Cell> newRoom,out List<Cell>cellsCheck);
+                        _rooms.Add(newRoom);
+                        foreach (Cell cell in cellsCheck) { 
+                            cellTraiter.Add(cell);
+                        }
+                    }
+                }
+                
+            }
+        }
+
+        foreach (List<Cell> room in _rooms) {
+            foreach (Cell cell in room)
+            {
+                cell.Material.color = color;
+            }
+
+            if (color == Color.blue)
+            {
+                color=Color.red;
+            }
+            else if (color == Color.red)
+            {
+                color = Color.green;
+            }else if(color ==Color.green)
+            {
+                color = Color.blue;
+            }
+            
+        }
+    }
+
+    private void RoomAnaliser(Cell cell , out List<Cell> roomCell,out List<Cell>cellCheck)
+    {
+        roomCell = new List<Cell>();
+        cellCheck=new List<Cell>();
+        roomCell.Add(cell);
+        Debug.Log("Anilise de la room");
+        
+        while (roomCell.Count>=cellCheck.Count)
+        {
+            Debug.Log(roomCell.Count+" case font partie de la salle   "+cellCheck.Count+"   On était traiter");
+            
+            List<Cell> newRoomCells= new List<Cell>();
+            foreach (Cell analisCell in roomCell)
+            {
+                Debug.Log("la casse traiter a comme coordoner" + analisCell.Position);
+                if (!cellCheck.Contains(analisCell))
+                {
+                    if (analisCell.Position.y+1 <MapXSize)
+                    {
+                        if (Matrix[analisCell.Position.x, analisCell.Position.y + 1].Material.color == Color.white && !roomCell.Contains(Matrix[analisCell.Position.x, analisCell.Position.y + 1])&& !newRoomCells.Contains(Matrix[analisCell.Position.x, analisCell.Position.y + 1]))
+                        {
+                            newRoomCells.Add(Matrix[analisCell.Position.x, analisCell.Position.y]);
+                        }
+                    }
+
+                    if (analisCell.Position.y - 1 >= 0)
+                    {
+                        if (Matrix[analisCell.Position.x, analisCell.Position.y - 1].Material.color == Color.white && !roomCell.Contains(Matrix[analisCell.Position.x, analisCell.Position.y - 1])&& !newRoomCells.Contains(Matrix[analisCell.Position.x, analisCell.Position.y - 1]))
+                        {
+                            newRoomCells.Add(Matrix[analisCell.Position.x, analisCell.Position.y]);
+                        }
+                    }
+
+                    if (analisCell.Position.x + 1 < MapZSize)
+                    {
+                        if (Matrix[analisCell.Position.x + 1, analisCell.Position.y].Material.color == Color.white && !roomCell.Contains(Matrix[analisCell.Position.x + 1, analisCell.Position.y])&& !newRoomCells.Contains(Matrix[analisCell.Position.x+1, analisCell.Position.y ]))
+                        {
+                            newRoomCells.Add(Matrix[analisCell.Position.x, analisCell.Position.y]);
+                        }
+                    }
+
+                    if (analisCell.Position.x - 1 >= 0)
+                    {
+                        if (Matrix[analisCell.Position.x - 1, analisCell.Position.y - 1].Material.color == Color.white && !roomCell.Contains(Matrix[analisCell.Position.x - 1, analisCell.Position.y])&& !newRoomCells.Contains(Matrix[analisCell.Position.x-1, analisCell.Position.y ]))
+                        {
+                            newRoomCells.Add(Matrix[analisCell.Position.x, analisCell.Position.y]);
+                        }
+                    }
+
+                    cellCheck.Add(analisCell);
+                }
+            }
+
+            foreach (Cell newRoomCell in newRoomCells)
+            {
+                roomCell.Add(newRoomCell);
+            }
+            newRoomCells.Clear();
+        }
+    }
+}
+
+
+
+public class Cell
+{
+    public Vector2Int Position;
+    public GameObject GameObject;
+    public Material Material;
+
+    public Cell(Vector2Int position, GameObject gameObject)
+    {
+        Position = position;
+        GameObject = gameObject;
+        Material = gameObject.GetComponent<MeshRenderer>().material;
+    }
 }
--- Assets/Scripte/PerlinNoise.cs
+++ Assets/Scripte/PerlinNoise.cs
@@ -3,7 +3,7 @@
 namespace Utils {
     public static class PerlinNoise {
     
-        public static float[,] Cave(float[,] map, float modifier, bool edgesAreWalls) {
+        public static float[,] Cave(float[,] map, float modifier, bool edgesAreWalls,Vector2 offset) {
             for (int x = 0; x < map.GetUpperBound(0); x++) {
                 for (int y = 0; y < map.GetUpperBound(1); y++) {
                     if (edgesAreWalls && (x == 0 || y == 0 || x == map.GetUpperBound(0) - 1 || y == map.GetUpperBound(1) - 1)) {
@@ -11,7 +11,8 @@ namespace Utils {
                     }
                     else {
                         //Generate a new point using Perlin noise, then round it to a value of either 0 or 1
-                        map[x, y] = Mathf.PerlinNoise(x * modifier, y * modifier);
+                        map[x, y] = Mathf.PerlinNoise(x * modifier+offset.x
+                            , y * modifier+offset.y);
                     }
                 }
             }
